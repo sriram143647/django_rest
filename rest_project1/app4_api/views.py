851542eb8d04ce37d6061ee99c0a2444a12036db
sriram143647 <kusumasriram143@gmail.com>
2022-12-27 15:40:16 +0530

    concrete-apiview-crud

--- rest_project1/app4_api/views.py
+++ rest_project1/app4_api/views.py
@@ -1,40 +1,12 @@
 from app4_api.models import student_data
 from app4_api.serializers import student_serializer
-from rest_framework.generics import ListAPIView,CreateAPIView,UpdateAPIView,RetrieveAPIView,DestroyAPIView,ListCreateAPIView,RetrieveDestroyAPIView,RetrieveUpdateAPIView,RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 
 # Create your views here.
-class student_list(ListAPIView):
-    queryset = student_data.objects.all()
-    serializer_class = student_serializer
-    
-class student_create(CreateAPIView):
-    queryset = student_data.objects.all()
-    serializer_class = student_serializer
-    
-class student_retrieve(RetrieveAPIView):
-    queryset = student_data.objects.all()
-    serializer_class = student_serializer
-    
-class student_update(UpdateAPIView):
-    queryset = student_data.objects.all()
-    serializer_class = student_serializer
-    
-class student_delete(DestroyAPIView):
-    queryset = student_data.objects.all()
-    serializer_class = student_serializer
-    
 class student_list_create(ListCreateAPIView):
     queryset = student_data.objects.all()
     serializer_class = student_serializer
-    
-class student_retrieve_destroy(RetrieveDestroyAPIView):
-    queryset = student_data.objects.all()
-    serializer_class = student_serializer
-    
-class student_retrieve_update(RetrieveUpdateAPIView):
-    queryset = student_data.objects.all()
-    serializer_class = student_serializer
-    
+        
 class student_retrieve_update_delete(RetrieveUpdateDestroyAPIView):
     queryset = student_data.objects.all()
     serializer_class = student_serializer
